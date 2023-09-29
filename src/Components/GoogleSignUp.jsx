fd02f4b48911b4532a370d1139723f5dd2512f21
mhmdNoman <mhmdnoman01@gmail.com>
2023-09-29 19:21:21 +0600

    added google signup

--- src/Components/GoogleSignUp.jsx
+++ src/Components/GoogleSignUp.jsx
@@ -1,7 +1,31 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "../Firebase_config";
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+const signUp = () => {
+  signInWithPopup(auth, provider)
+    .then((res) => {
+      const user = res.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+};
+
 const GoogleSignUp = () => {
   return (
-<div>GoogleSignUp</div>
-  )
+    <div className="mt-10 flex justify-center w-full">
+      <button className="btn btn-success text-white" onClick={signUp}>
+        Sign up
+      </button>
+    </div>
+  );
 };
+
+app;
 
 export default GoogleSignUp;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <div>Welcome to login with email, google and github.</div>;
+  return (
+    <div className="w-full">
+      <h1 className="text-center mt-16 text-3xl">
+        Welcome to login with email, google and github.
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
